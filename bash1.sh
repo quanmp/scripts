4be4fc4bb5eb2932191e21a87cef3748202e0326
quanmp <quanmp@gmail.com>
2016-01-14 02:10:03 -0500

    bash at work

---
+++ bash1.sh
@@ -0,0 +1,11 @@
+## credit:<http://www.cyberciti.biz/faq/how-do-i-find-the-largest-filesdirectories-on-a-linuxunixbsd-filesystem/> 
+## Find large files in linux
+# du -a /var | sort -n -r | head -n 10
+
+
+## rsync example
+# rsync options source destination
+
+## Find open file descriptor of a process
+# lsof -p PID
+# ls -l /proc/PID/fd
